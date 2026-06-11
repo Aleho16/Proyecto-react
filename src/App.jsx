180774528752571aa0b97aeb3da6dc81566daee5
Aleho16 <perez.alejo.rojas@gmail.com>
2026-06-11 14:45:13 -0500

    se añade 'value' al componente Square

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './styles.css';
 
 import App from './App';
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 export default function Board() {
   return (
